4ed2b5a1a438b3f4766ae08c0e5016350e799d71
naman0105 <naman0105@gmail.com>
2018-04-28 00:36:54 +0530

    removed unnessasary stuff

--- public/book.js
+++ public/book.js
@@ -1,10 +1,6 @@
 angular.module('booksapp', [])
 .controller('books', function($scope, $http) {
  // Initialize variables
- $scope.key = 'fds';
- this.name2 = '';
- $scope.greeting1 = `Hello ${$scope.name1}`;
- this.greeting2 = `Hi ${this.name2}`;
  $http.get('/books').then(function(response){
     console.log(response);
     $scope.booklist = response;
